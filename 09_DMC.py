b7049e4f76bd53d9c3d2da417fa7f09b25d9f880
hariskhan2010 <harismalik2010afridi@gmail.com>
2025-08-26 11:11:16 +0500

    Add files via upload

---
+++ 09_DMC.py
@@ -0,0 +1,72 @@
+# # student_name = input("enter your name:- ").capitalize()
+
+# # islamiyat = int(input("Islamiat Marks = "))
+# # maths = int(input("Maths Marks = "))
+# # science = int(input("science Marks = "))
+# # english = int(input("english Marks = "))
+# # mataliyaquran= int(input("Mataliyaquran Marks = "))
+# # urdu = int(input("urdu Marks = "))
+# # socialstudy = int(input("socialstudy Marks = "))
+# # computer = int(input("computer Marks = "))
+
+
+# # print("student_name:", student_name)
+# # if islamiyat <= 100:
+# #     print("islamiyat", islamiyat)
+# # if maths <= 100:
+# #     print("maths", maths)
+# # if science <= 100:
+# #     print("science", science)
+# # if english <= 100:
+# #     print("english", english)
+# # if mataliyaquran <= 100:
+# #     print("mataliyaquran", mataliyaquran)
+# # if urdu <= 100:
+# #     print("urdu", urdu)
+# # if socialstudy <= 100:
+# #     print("socialstudy", socialstudy)
+# # if computer <= 100:
+# #     print("computer", computer)
+
+# # total_marks = 800
+# # print("Total Marks: ", total_marks)
+
+# # obtained_marks = islamiyat + maths + science + english + mataliyaquran + urdu + socialstudy + computer
+# # print("Obtained Marks", obtained_marks)
+
+# # percentage = obtained_marks * 100 // total_marks
+# # print("percentage", percentage)
+
+# # if percentage >= 90:
+# #     print("Grade A+")
+# # elif percentage >= 80:
+# #     print("Grade A")
+# # elif percentage >= 70:
+# #     print("Grade B")
+# # elif percentage >= 60:
+# #     print("Grade C")
+# # elif percentage >= 50:
+# #     print("Grade D")
+# # elif percentage < 50:
+# #     print("Fail")
+# # else:
+# #     print("incorrect")
+
+# # if percentage >= 90:
+# #     print("Excellent")
+# # elif percentage >= 80:
+# #     print("Outstanding")
+# # elif percentage >= 70:
+# #     print("Good")
+# # elif percentage >= 60:
+# #     print("Satisfine")
+# # elif percentage >= 50:
+# #     print("Need improvement")
+# # elif percentage < 50:
+# #     print("Try again")
+# # else:
+# #     print("invalid percentage")
+
+
+
+
